6dfa56d4b584a98bd7d21a314f17a5d54f7c74be
Lucas Khenayfis <lucaskhenayfis@gmail.com>
2021-10-04 14:20:30 -0300

    Paraleliza extracao dos dados

--- data-raw/extraidados.r
+++ data-raw/extraidados.r
@@ -1,6 +1,3 @@
-devtools::load_all()
-
-# extrai historicos horarios -----------------------------------------------------------------------
 root <- "//rio-arq-01/_GTDP/Ciclo 2 - 2010 a 2019/_Resultados Finais por Usina"
 dirs <- list.dirs(root, full.names = TRUE, recursive = FALSE)
 dirs <- file.path(dirs, "Vazao Turbinada")
@@ -8,13 +5,20 @@ dirs <- file.path(dirs, "Vazao Turbinada")
 arqs <- sapply(dirs, list.files, full.names = TRUE, pattern = "^Processo.*xlsm$")
 arqs <- unlist(unname(arqs))
 
-for(arq in arqs) {
+clst <- parallel::makeCluster(7, "PSOCK")
+parallel::clusterExport(clst, c("arqs"))
+parallel::clusterEvalQ(clst, devtools::load_all("C:/Users/lucask/Documents/GTDP/perdaprodvar"))
+
+parallel::parLapply(clst, arqs, function(arq) {
 
     dado <- leplanilha(arq)
 
-    outdir <- sub("Vazao.*", "Produtibilidade Variável", arq)
+    outdir <- sub("Vazao.*", "Produtibilidade VariÃ¡vel", arq)
+    usina  <- gsub("(.*iterativo - )|(\\.xls[xm])", "", arq)
     if(!dir.exists(outdir)) dir.create(outdir)
 
-    data.table::fwrite(dado, file.path(outdir, "dadohorario.csv"), quote = FALSE)
-    saveRDS(dado, file.path(outdir, "dadohorario.RDS"))
-}
+    data.table::fwrite(dado, file.path(outdir, paste0(usina, ".csv")), quote = FALSE)
+    saveRDS(dado, file.path(outdir, paste0(usina, ".RDS")))
+})
+
+parallel::stopCluster(clst)
