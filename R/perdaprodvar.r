fd558d1ba82b3b68ee087ebc65566529add7e5c1
Lucas Khenayfis <lucaskhenayfis@gmail.com>
2021-10-04 12:18:52 -0300

    Init repo (fork de montagridsons)

---
+++ R/perdaprodvar.r
@@ -0,0 +1,13 @@
+#' Modelagem De Perdas e Produtibilidades Variaveis
+#' 
+#' @docType package
+#' @name DCS
+#' 
+#' @import data.table
+NULL
+
+#' ## usethis namespace: start
+#' @useDynLib perdaprodvar, .registration = TRUE
+#' @importFrom Rcpp sourceCpp
+#' ## usethis namespace: end
+NULL
