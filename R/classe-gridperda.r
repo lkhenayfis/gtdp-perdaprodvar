0664adce77e8002f74a116489b862eb368bb6d41
Lucas Khenayfis <lucaskhenayfis@gmail.com>
2021-10-05 14:21:43 -0300

    Add script da classe gridperda

--- R/classe-gridperda.r
+++ R/classe-gridperda.r
@@ -1,6 +1,6 @@
 ######################################### CLASSE GRIDPERDA #########################################
 
-extraigrade.gamperda <- function(fit, dim, ...) {
+extraigrid.gamperda <- function(fit, dim, ...) {
 
     dim <- dim[1]
 
@@ -16,4 +16,33 @@ new_gridperda <- function(perda, vazao, fit) {
 
     out <- list(grid = gridperda, gam = fit)
     class(out) <- "gridperda"
+
+    return(out)
+}
+
+print.gridperda <- function(x, ...) print(gg$grid)
+
+# METODOS ------------------------------------------------------------------------------------------
+
+predict.gridperda <- function(object, newdata, ...) {
+
+    grid  <- object$grid
+    setorder(grid, vazao)
+
+    vazao <- sort(newdata$vazao)
+
+    interp <- INTERPLIN(grid$vazao, grid$perda, vazao)
+
+    return(interp)
+}
+
+reiduals.gridperda <- function(object, ...) {
+
+    vazao <- object$gam$dat$vazao
+
+    fit <- predict(object, newdata = data.frame(vazao = vazao))
+
+    res <- object$gam$dat$perda - fit
+
+    return(res)
 }
