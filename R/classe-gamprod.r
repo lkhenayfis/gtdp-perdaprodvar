55f372af8db6abde8b79ade47ddd82bc2f151fbd
Lucas Khenayfis <lucaskhenayfis@gmail.com>
2021-10-06 15:22:15 -0300

    Evolui metodos de gamprod

--- R/classe-gamprod.r
+++ R/classe-gamprod.r
@@ -15,7 +15,7 @@ NULL
 
 new_gamprod <- function(dat, mod, atributos, args) {
 
-    dat <- copy(dat)[, .SD, .SDcols = c("quedal", "vazao", "perda")]
+    dat <- copy(dat)[, .SD, .SDcols = c("quedal", "vazao", "prod")]
 
     out <- list(model = mod, dat = dat)
     attr(out, "gamargs") <- args
@@ -31,3 +31,50 @@ print.gamprod <- function(x, ...) {
     cat("\n## GAM\n")
     print(x$model)
 }
+
+# METODOS ------------------------------------------------------------------------------------------
+
+#' @param object objeto \code{gamprod} retornado por \code{\link{fitgam_prod}}
+#' @param newdata data.frame ou data.table contendo variável explicativa para previsão
+#' @param ... demais parametros
+#' 
+#' @return para \code{predict.gamprod}, vetor de produtibilidades previstas nas abscissas contidas 
+#'     em \code{newdata}. Para code{fitted.gamprod}, os valores ajustados e \code{rediduals.gamprod} 
+#'     os erros
+#' 
+#' @export
+#' 
+#' @rdname gamprod
+
+predict.gamprod <- function(object, newdata, ...) {
+
+    pred <- predict(object$model, newdata = newdata)
+
+    return(pred)
+}
+
+#' @export
+#' 
+#' @rdname gamprod
+
+fitted.gamprod <- function(object, ...) predict(object, object$dat)
+
+#' @export
+#' 
+#' @rdname gamprod
+
+residuals.gamprod <- function(object, ...) {
+
+    fit <- fitted(object)
+    res <- object$dat$prod - fit
+
+    return(res)
+}
+
+#' @export
+
+AIC.gamprod <- function(object, ...) AIC(object$model)
+
+#' @export
+
+BIC.gamprod <- function(object, ...) BIC(object$model)
