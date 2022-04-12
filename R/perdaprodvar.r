d6a64fc385c9a374524881507cde5f20a5cea2b0
Lucas Khenayfis <lucaskhenayfis@gmail.com>
2022-04-12 15:32:27 -0300

    Melhora documentacao do titulo do pacote

--- R/perdaprodvar.r
+++ R/perdaprodvar.r
@@ -1,11 +1,19 @@
-#' Modelagem De Perdas e Produtibilidades Variaveis
+#' \code{perdaprodvar}
+#' 
+#' Modelagem De Dados De Perda E Produtibillidade E Extração De Grades
+#' 
+#' Este pacote contém o conjunto de funções necessárias para aplicação e análise da metodologia para
+#' modelagem de perda e produtibillidade variáveis através de modelos aditivos. Além de funções
+#' dedicadas à extração dos dados e cálculo das médias semanais, são fornecidas funções para ajuste
+#' dos modelos, otimização dos hiperparâmetros dos mesmos, extração de grades e otimização do número
+#' de segmentações necessárias, segundo critérios acordados no GT Produtibillidade Hidrelétrica da
+#' CPAMP. Finalmente, funções para visualização e diagnósticos são inclusas.
 #' 
 #' @docType package
 #' @name perdaprodvar
 #' 
-#' @import data.table
-NULL
-
 #' @useDynLib perdaprodvar
 #' @importFrom Rcpp sourceCpp
+#' 
+#' @import data.table
 NULL
