0941229242912f559257b2a398955285c0b11ecb
Lucas Khenayfis <lucaskhenayfis@gmail.com>
2022-04-17 12:34:06 -0300

    Inclui otimizador de dimensoes de base p prod

--- R/optgam.r
+++ R/optgam.r
@@ -48,24 +48,57 @@ optgam_perda <- function(dat, range.vazao = 5:30, ts.vazao = "ps", extrap = c(2,
 
 #' Otimização Do Ajuste De Produtibilidade
 #' 
-#' Varre uma faixa de dimensões de base, reportando aquele correspondente ao ajuste de menor BIC
+#' Varre uma faixa de dimensões de base, reportando aquela correspondente ao ajuste de menor BIC
+#' 
+#' Os parâmetros recebidos por \code{optgam_prod} são exatamente aqueles de 
+#' \code{\link{fitgam_prod}}, com uma exceção: os argumentos \code{ns.quedal} e \code{ns.vazao} são
+#' substituídos por \code{range.quedal} e \code{range.vazao}, vetores de inteiros indicando as 
+#' dimensões de base candidatas em cada marginal para seleção.
+#' Mais detalhes a respeito da estimação e efeito dos argumentos individuais podem ser encontrados 
+#' em \code{\link{fitgam_prod}}
+#' 
+#' @param dat \code{data.table} de dados para ajuste. Ver Detalhes
+#' @param range.quedal,range.vazao vetor de dimensões de base a testar na seleção. Todas as
+#'     combinações serão testadas (isto é, um domínio quadrado é gerado a partir dos ranges). Padrão
+#'     \code{range.quedal = range.vazao = 5:30}
+#' @param ts.quedal,ts.vazao tipo de spline utilizada para queda líquida e vazão -- 
+#'     veja \code{\link[mgcv]{smooth.terms}} para todas as opções. Padrão \code{"ps"} em ambos
+#' @param bordas booleano indicando o uso ou não de bordas; alternativamente, um vetor de inteiros 
+#'     indicando quais vértices utilizar. Ver Detalhes
+#' @param modo um de \code{"tensor"}, \code{"multivar"} ou \code{"simples"} indicando o modo de 
+#'     interação entre funções marginais. Ver Detalhes
+#' 
+#' @examples 
+#' 
+#' dat <- agregasemana(dummydata)
+#' 
+#' # execucao limitando a faixa de numero de splines a algo mais baixo
+#' optfit <- optgam_prod(dat, range.quedal = 5:15, range.vazao = 5:15)
+#' plot(optfit)
+#' 
+#' # dentre os argumentos de optfit esta a lista 'gamargs', contendo a parametrizacao do gam
+#' attr(optfit, "gamargs") # ns.vazao corresponde ao numero de nos utilizados no ajuste otimo
+#' 
+#' @return objeto \code{gamprod} contendo GAM e extrapolações estimadas
+#' 
+#' @seealso ajuste individual para uma dada dimensão de base \code{\link{fitgam_prod}}
+#' 
+#' @family metodos gamprod
+#' @family plots gamprod
+#' 
+#' @export
 
-optgam_prod <- function() {
+optgam_prod <- function(dat, range.quedal = 5:30, range.vazao = 5:30, ts.quedal = "ps", ts.vazao = "ps",
+    bordas = TRUE, modo = c("tensor", "multivar", "simples")) {
 
-    return(NULL)
-}
+    ranges <- expand.grid(quedal = range.quedal, vazao = range.vazao)
 
-# HELPERS ------------------------------------------------------------------------------------------
+    fitgams <- lapply(seq(nrow(ranges)), function(i)
+        fitgam_prod(dat, ranges[i, 1], ranges[i, 2], ts.quedal, ts.vazao, bordas, modo)
+    )
+    BICs <- sapply(fitgams, BIC)
 
-matchattr <- function(obj1, obj2, quais = c("cod", "ts")) {
+    optgam <- fitgams[[which.min(BICs)]]
 
-    quais <- paste0("(", paste0(quais, collapse = ")|("), ")")
-
-    attr1 <- attributes(obj1)
-    attr2 <- attributes(obj2)
-
-    attr1 <- attr1[grep(quais, names(attr1))]
-    attr2 <- attr2[grep(quais, names(attr2))]
-
-    all(mapply(attr1, attr2, FUN = "=="))
+    return(optgam)
 }
